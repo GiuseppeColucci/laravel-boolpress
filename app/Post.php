d722a84268f66569a9b88d63e047f9b28231e791
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-03-31 00:05:56 +0200

    definito la relazione nei model mancano i tag con i seed

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable =['title','body','author_id'];
+
     public function author(){
         return $this->belongsTo('App\Author');
     }
+
+    public function tags(){
+        return $this->belongsToMaby('App\Tag');
+    }
+
 }
